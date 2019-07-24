bd1e542e002f706c71b6ef1d530be0c3b53b5ae6
GTX1050 <ojhaamir007@gmail.com>
2019-07-24 23:39:18 +0545

    Vuetify design

--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
 import Vue from "vue";
 import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import router from "./route/router";
+import store from "./store/store";
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
+import VueI18n from "vue-i18n";
+import FlagIcon from "vue-flag-icon";
 
 Vue.config.productionTip = false;
+Vue.use(Vuetify);
+Vue.use(VueI18n);
+Vue.use(FlagIcon);
 
 new Vue({
   router,
