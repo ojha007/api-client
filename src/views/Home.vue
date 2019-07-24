695538a3761fc350035684ed433090724de3bbce
GTX1050 <ojhaamir007@gmail.com>
2019-07-24 23:50:15 +0545

    added card

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,21 @@
 <template>
   <div class="home">
-    <h1>This is Home</h1>
+    <v-container>
+      <v-flex>
+        <v-card class="mx-auto" outlined="true" media width="200" height="100" absolute>
+          <v-img
+            class="white--text"
+            height="200px"
+            src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+          >
+            <v-card-title class="align-end fill-height"
+              >I'm a title</v-card-title
+            >
+          </v-img>
+          <v-card-title>I'm a title</v-card-title>
+        </v-card>
+      </v-flex>
+    </v-container>
   </div>
 </template>
 
