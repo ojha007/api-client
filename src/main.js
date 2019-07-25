796ade1345b68961e09b095abb735382917109e7
GTX1050 <ojhaamir007@gmail.com>
2019-07-25 11:59:57 +0545

    office

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from "vue";
 import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-
+import router from "./route/router";
+import store from "./store/store";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import VueI18n from "vue-i18n";
+import FlagIcon from "vue-flag-icon";
+import "flag-icon-css/css/flag-icon.css";
+Vue.use(FlagIcon);
+Vue.use(VueI18n);
 Vue.config.productionTip = false;
 
 new Vue({
